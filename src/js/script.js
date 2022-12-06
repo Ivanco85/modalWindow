b3a3a35017a5a2d881eebb9b1e5142265c57ce16
Ivanco <ivan.wasilev21@gmail.com>
2022-12-06 17:35:05 +0300

    06.12.22

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
-const $ = {}
+// const $ = {
+//     testFunction:function(num) {
+//         console.log(num)
+//     }
+// }
+const modal = document.querySelector('.modal-overlay')
+window.addEventListener('keydown', function(event) {
+    if (event.code === 'Space') {
+        modal.removeAttribute('hidden')
+    }
+
+}) 
+
+const modalClosebtn = document.querySelector('.modal-close')
+modalClosebtn.addEventListener('click', function(){
+    modal.setAttribute('hidden','')
+})
